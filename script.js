3cfebbc98db84792f81b7deb39dc6e24b2f478ab
philipheferreira <philipheferreira@gmail.com>
2023-05-10 17:43:20 -0300

    update display do rgb cores

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.querySelector(".teste").innerHTML = 'Testando';
+const valorEntrada = document.querySelector(".valorInput");
+
+const valorBotao = document.querySelector(".botaoGerarSenha");
+
+
+valorBotao.addEventListener("click", function(){ 
+	console.log("valor botao clicado");
+	valorEntrada.value = "testado";
+})
